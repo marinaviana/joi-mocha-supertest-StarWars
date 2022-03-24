46f5fbb741ccaceac5970bd2225e87c955ce0c53
marinaviana <marina.s.viana@gmail..com>
2022-03-24 19:51:50 -0300

    chore: change version joi

--- test/requests/pessoas.js
+++ test/requests/pessoas.js
@@ -1,28 +1,19 @@
 const request = require('supertest');
-const Joi = require('joi');
-const joiAssert = require('joi-assert');
-const expect = require('chai').expect;
 
 const{
     schemaPessoasValido,
 } = require('../schemas/pessoasSchema.js');
 
-const URL = 'https://swapi.co/api/';
+const URL = 'https://swapi.dev/api/';
 const PATH = 'people/1/';
 
-const request_timeout = 20000;
+describe('Teste Contrato API', async() => {
 
-it('Validando response com Joi.validate', function(done){
-    request(URL)
-    .get(PATH)
-    .expect('Content-Type', /json/)
-    .end(function(err, res){
-        expect(res.status).to.eql(200);
-        Joi.validate(res.body, schemaPessoasValido,{
-            abortEarly: false
-        },(err, data) => {
-            if(err) throw err;
-        });
-        done(err);
-    })
+it('Validando response com Joi.validate', async() => {
+    const { body } = await request(URL)
+    .get(PATH);
+    
+    await schemaPessoasValido.validateAsync(body);
+
 });
+})
